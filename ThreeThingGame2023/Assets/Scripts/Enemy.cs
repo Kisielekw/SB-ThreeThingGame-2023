c6b2e29683a7c1ade5488790f35f98b308e12387
Kisielekw <dawkis1@outlook.com>
2023-11-25 17:40:38 +0000

    Added a lot of scripts

---
+++ ThreeThingGame2023/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private bool pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPause(bool pPause)
+    {
+        pause = pPause;
+    }
+}
--- ThreeThingGame2023/Assets/Scripts/SpawnPowerPlants.cs
+++ ThreeThingGame2023/Assets/Scripts/SpawnPowerPlants.cs
@@ -12,9 +12,12 @@ public class SpawnPowerPlants : MonoBehaviour
 
     private float lastSpawnTime;
 
+    private bool pause;
+
     // Start is called before the first frame update
     void Start()
     {
+        pause = false;
         lastSpawnTime = 0;
 
         for(int i = 0; i <= 26; i++)
@@ -26,10 +29,18 @@ public class SpawnPowerPlants : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (pause)
+            return;
+
         if(Time.time - lastSpawnTime >= timeInterval)
         {
             Instantiate(powerPlant, spawnPoints[Random.Range(0, 26)], Quaternion.identity);
             lastSpawnTime = Time.time;
         }
+    }
+
+    public void SetPasue(bool pPause)
+    {
+        pause = pPause;
     }
 }
